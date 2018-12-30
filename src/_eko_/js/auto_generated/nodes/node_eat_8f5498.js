b9dc04b33d0d4cf99982f2999627033e3854bca8
Eko Studio <>
2018-12-30 12:20:23 +0000

    vaddes

--- src/_eko_/js/auto_generated/nodes/node_eat_8f5498.js
+++ src/_eko_/js/auto_generated/nodes/node_eat_8f5498.js
@@ -1,13 +1,15 @@
 import ivds from '../ivds.js';
 export default {
     id: 'node_eat_8f5498',
-    source: ivds.vid_video_placeholder_4sec,
+    source: ivds.vid_video_blank_dbb67d,
     data: {
                     insights: {
                 uid: 'e44285d0-0c2c-11e9-b919-f1e78db08c26'
                             },
                             decision: {
                 startTime: 1,
+                 
+                    endTime: 6.022,
                                 children: [
                                 ],
                 defaults: [
