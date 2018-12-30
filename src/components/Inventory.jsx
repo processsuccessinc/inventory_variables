3e9c2b31cb4c10937c25d69a84d4dcb752930456
Ran Gliksberg <ran.gliksberg@helloeko.com>
2018-12-30 16:16:56 +0200

    iterate inventory items/ decide final node

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -4,19 +4,21 @@ export default class Inventory extends React.Component {
     render() {
         // For this example we have two
         let inventory = player.variables.getValue('inventory');
-        let inventoryBody = inventory === 1 ? 
-        <div>
-            <ul>
-                <li>Clemantine</li>
-            </ul>
-        </div> 
-        : 
-        <div></div>;
+        let inventoryContents = [];
+        inventory.forEach(element => {
+            inventoryContents.push(
+            <div>
+                <ul>
+                    <li>{element}</li>
+                </ul>
+            </div>
+            );
+        });
 
         return (
-            <div style={{ float: 'right', position: 'absolute', top: '35%', right: '30%', border: 'solid', height: '30%', width: '25%' }}>
+            <div id="inventory" style={{ float: 'right', position: 'absolute', top: '35%', right: '30%', border: 'solid', height: '30%', width: '25%' }}>
                 <div style={{ padding: '6%' }}><h2>Inventory</h2></div>
-                <div style={{ padding: '6%' }}>{inventoryBody}</div>
+                <div style={{ padding: '6%' }}>{inventoryContents}</div>
             </div>
         );
     }
--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,42 @@
 import playerOptions from '../config/playerOptions.json';
-// import Inventory from '../components/Inventory';
+import Inventory from '../components/Inventory';
 
 export default {
     onLoad: function(ctx) {},
 
     onInit: function(player, ctx) {
-        // player.ui.add('inventory', Inventory);
-        // player.variables.register({
-        //     name: 'inventory',
-        //     initialValue: 0,
-        //     updateOn: 'nodestart',
-        //     resetOn: 'playlistreset',
-        //     dataSet: {
-        //         node_blank_613ae5: 1,
-        //         node_fruits_on_table_2430df: 0
-        //     }
-        // });
+        player.ui.add('inventory', Inventory);
+        player.variables.register({
+            name: 'inventory',
+            initialValue: [],
+            updateOn: 'nodestart',
+            resetOn: 'playlistreset',
+            dataSet: {
+                node_banana_left_613ae5: ['Orange'],
+                node_orange_left_cf47c4: ['Banana'],
+                node_fridge_empty_70b503: ['Orange', 'Banana']
+            }
+        });
+
+        player.decision.add('finalFruitsInPlate', {
+            parent: 'node_eat_8f5498',
+            endTime: -4,
+            decider: function(parent, children, options) {
+                let inventory = player.variables.getValue('inventory');
+                let orange = inventory.include('orange');
+                let banana = inventory.include('banana');
+                if(orange && banana) {
+                    return 'node_orange_and_banana_on_plate_d91bab';
+                } else if(orange) {
+                    return 'node_orange_on_plate_4c07f4';
+                } else if(banana) {
+                    return 'node_banana_on_plate_f573b4';
+                } else {
+                    return 'node_empty_plate_6342bf';
+                }
+
+            }
+        });
         
     },
     
